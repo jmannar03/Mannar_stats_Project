d9b9a0f87946f94c7bb1050253eb2ca70c8b429a
JustinMannar <Justin.mannar@gmail.com>
2024-04-03 19:59:42 -0700

    admin setup

--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(StarWarsCharacter)
+admin.site.register(Project)
 
 # Register your models here.
--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    has_DataSet = models.BooleanField(default = False)
+    about = models.CharField(max_length=200, null=True, blank = True)
+    email = models.CharField(max_length=200)
+
+class StarWarsCharacter(models.Model):
+    name = models.CharField(max_length=100)
+    height = models.FloatField(null=True)
+    mass = models.FloatField(null=True)
+    hair_color = models.CharField(max_length=50, null=True)
+    skin_color = models.CharField(max_length=50, null=True)
+    eye_color = models.CharField(max_length=50, null=True)
+    birth_year = models.CharField(max_length=20, null=True)
+    gender = models.CharField(max_length=10, null=True)
+    homeworld = models.CharField(max_length=100, null=True)
+    species = models.CharField(max_length=100, null=True)
+    films = models.JSONField(null=True)
+    vehicles = models.JSONField(null=True)
+    starships = models.JSONField(null=True)
+
+
 # Create your models here.
