ee6d980755cc23af28840c7022a87a3e919a4622
JustinMannar <Justin.mannar@gmail.com>
2024-04-06 12:01:03 -0700

    everything but view form done

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
     path('characters/',views.current_Characters,name='characters'),
     path('projects/<int:pk>', views.ProjectListView.as_view(), name = 'project-detail'),
     path('characters/<int:pk>', views.CharacterListView.as_view(), name = 'character-list'),
+    path('create_Character/', views.add_Character, name='create_Character'),
+    path('create_Project/',views.add_Project,name='create_Project'),
+    path('delete_Project/<str:pk>/', views.delete_Project, name= 'delete_Project'),
+    path('delete_Character/<str:pk>/', views.delete_Character, name= 'delete_Character'),
+    path('update_Character/<str:pk>/', views.update_Character, name= 'update_Character'),
+    path('update_Project/<str:pk>/', views.update_Project, name= 'update_Project'),
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from django.views import generic
+from .forms import *
 
 # Create your views here.
 def index(request):
@@ -25,3 +26,77 @@ class ProjectListView(generic.DetailView):
 
 class CharacterListView(generic.DetailView):
     model = StarWarsCharacter
+
+def add_Character(request):
+    form = CharacterForm
+    if request.method == 'POST':
+        #print('Printing POST: ',request.POST)
+        form = CharacterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request, 'my_project/create_form.html',context)
+
+def add_Project(request):
+    form = ProjectForm
+    if request.method == 'POST':
+        #print('Printing POST: ',request.POST)
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request, 'my_project/crete_project_form.html',context)
+
+def delete_Project(request,pk):
+    project = Project.objects.get(id=pk)
+    if request.method == "POST":
+        project.delete()
+        return redirect('/')
+    context = {'item':project}
+    return render(request, 'my_project/delete_project.html',context)
+
+
+def delete_Character(request,pk):
+    character = StarWarsCharacter.objects.get(id=pk)
+    if request.method == "POST":
+        character.delete()
+        return redirect('/')
+    context = {'item':character}
+    return render(request, 'my_project/delete_character.html',context)
+
+def update_Project(request,pk):
+    project=Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        #print('Printing POST: ',request.POST)
+      form = ProjectForm(request.POST,instance=project)
+      if form.is_valid():
+         form.save()
+         return redirect('/')
+    context = {'form':form}
+    return render(request, 'my_project/update_project_form.html',context)
+
+def update_Character(request,pk):
+    character=StarWarsCharacter.objects.get(id=pk)
+    form = CharacterForm(instance=character)
+    if request.method == 'POST':
+        #print('Printing POST: ',request.POST)
+      form = CharacterForm(request.POST,instance=character)
+      if form.is_valid():
+         form.save()
+         return redirect('/')
+    context = {'form':form}
+    return render(request, 'my_project/update_character_form.html',context)
+
+def view_character(request):
+    return render(request, 'my_project/view_character.html')
+
+def view_project(request):
+    return render(request, 'my_project/view_project.html')
+
+
+
